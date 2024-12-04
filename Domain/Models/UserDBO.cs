4756587119a107976dfcbb7332f24c388329dc80
nkoro <nkorolev090@gmail.com>
2024-12-05 00:43:58 +0300

    add orm
    [lesson3]

---
+++ Domain/Models/UserDBO.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Models
+{
+    public class UserDBO
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Domain/Services/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Models;
+using Dapper;
+using Domain.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Domain.Services
+{
+    public class UserRepository: IUserRepository
+    {
+        private string connectionString;
+        public UserRepository(string conn)
+        {
+            connectionString = conn;
+        }
+
+        public async Task<UserDBO?> Get(string login, string password)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var queryResult = await db.QueryAsync<UserDBO>("SELECT * FROM [User] WHERE Login = @login AND Password = @password", new { login, password });
+                return queryResult.FirstOrDefault();
+            }
+        }
+    }
+}
